f52c4a7f27d5876df4bcef4fce583aadf21fe22f
tri-et <tripham178@gmail.com>
2020-02-21 12:07:56 +0700

    fix try catch function checkAttendance

--- lib/providers/user.dart
+++ lib/providers/user.dart
@@ -8,7 +8,7 @@ enum AttendanceType { In, Out }
 enum AttendanceStatus { CheckedIn, CheckedOut, NotCheckIn, NotCheckOut }
 
 class User with ChangeNotifier {
-  String _urlReports = "https://attendance-dcecd.firebaseio.com/reports/";
+  String _urlReports = "https://atapp-7720c.firebaseio.com/reports/";
   String _idToken; // Firebase ID token of the account.
   String _localId; // The uid of the current user.
   String email;
@@ -23,49 +23,54 @@ class User with ChangeNotifier {
     return "$_urlReports$_localId/$currentDateString.json?auth=$_idToken";
   }
 
-  Future<AttendanceStatus> checkAttendance() async {
-    AttendanceStatus message;
-    var reponseCheckText = await Util.fetch(FetchType.GET, urlRecordAttendance);
-    final responseData = json.decode(reponseCheckText);
-    if (responseData != null) {
-      if (responseData['error'] != null) throw (responseData['error']);
+  Future<Map<String, dynamic>> checkAttendance() async {
+    AttendanceStatus status;
+    var responseData;
+    try {
+      var reponseText = await Util.fetch(FetchType.GET, urlRecordAttendance);
+      responseData = json.decode(reponseText);
+    } catch (error) {
+      return {'error': error.toString()};
+    }
 
-      if (responseData['in'] != null) message = AttendanceStatus.CheckedIn;
-      if (responseData['out'] != null) message = AttendanceStatus.CheckedOut;
-      if (responseData['out'] == null) message = AttendanceStatus.NotCheckOut;
+    if (responseData != null) {
+      if (responseData['error'] != null) return {'error': responseData['error']};
+      if (responseData['in'] != null) status = AttendanceStatus.CheckedIn;
+      if (responseData['out'] != null) status = AttendanceStatus.CheckedOut;
+      if (responseData['out'] == null) status = AttendanceStatus.NotCheckOut;
     } else
-      message = AttendanceStatus.NotCheckIn;
-    return message;
+      status = AttendanceStatus.NotCheckIn;
+    return {'result': status};
   }
 
   Future<String> recordAttendance(AttendanceType attendanceType) async {
+    List<bool> lstChecked = await Future.wait([/*checkIP(), checkLocation()*/]);
+    if (!lstChecked[0]) return 'Wrong IP';
+    if (!lstChecked[1]) return 'Wrong Location';
+
+    final attendanceStatus = await checkAttendance();
+    if (attendanceStatus['error'] != null) return attendanceStatus['error'];
+
+    var responseText;
     try {
-      List<bool> lstChecked = await Future.wait([/*checkIP(), checkLocation()*/]);
-      if (!lstChecked[0]) return 'Wrong IP';
-      if (!lstChecked[1]) return 'Wrong Location';
-
-      final attendanceMsg = await checkAttendance();
-      var responseText;
-
       switch (attendanceType) {
         case AttendanceType.In:
-          if (attendanceMsg == AttendanceStatus.NotCheckIn) {
+          if (attendanceStatus['result'] == AttendanceStatus.NotCheckIn) {
             responseText = await Util.fetch(FetchType.PUT, urlRecordAttendance, {
               'in': DateTime.now().toString(),
             });
-          } else {
-            responseText = "{\"error\": \"Checked In Before !!!\"}";
-          }
+          } else
+            return "Checked In Before !!!";
           break;
         case AttendanceType.Out:
-          if (attendanceMsg == AttendanceStatus.NotCheckOut) {
+          if (attendanceStatus['result'] == AttendanceStatus.NotCheckOut) {
             responseText = await Util.fetch(FetchType.PATCH, urlRecordAttendance, {
               'out': DateTime.now().toString(),
             });
-          } else if (attendanceMsg == AttendanceStatus.CheckedOut)
-            responseText = "{\"error\": \"Checked Out Before !!!!\"}";
+          } else if (attendanceStatus['result'] == AttendanceStatus.CheckedOut)
+            return "Checked Out Before !!!!";
           else
-            responseText = "{\"error\": \"Not Check In !!!\"}";
+            return "Not Check In !!!";
           break;
       }
 
